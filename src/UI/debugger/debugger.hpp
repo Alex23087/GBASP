f0e9cabe7dd720cecd27cdb238e4869f8f425e9a
Sal <salerno.salvatore1997@gmail.com>
2025-04-23 00:18:29 +0200

    Fixed Cmake for linking the Qt Library (thanks Perkyello).
    Co-authored-by: Alessandro Scala <Alex23087@users.noreply.github.com>

--- src/UI/debugger/debugger.hpp
+++ src/UI/debugger/debugger.hpp
@@ -1,19 +1,33 @@
-<QApplication>
-#include <QPushButton>
-#include <QWidget>
+#include <QApplication>
 #include <QLabel>
+#include <QMainWindow>
 
-int main(int argc, char* argv[]) {
+int main_d(int argc, char* argv[]) {
     QApplication app(argc, argv);
 
-    QWidget window;
-    window.setWindowTitle("Hello World App");
+    // Create a main window
+    QMainWindow window;
 
-    QLabel label("Hi there!", &window);
-    label.setAlignment(Qt::AlignCenter);
+    // Create a label with "Hello World" text
+    QLabel* label = new QLabel("Hello World");
 
-    window.resize(400, 300);
+    // Set the font to be larger
+    QFont font = label->font();
+    font.setPointSize(20);
+    label->setFont(font);
+
+    // Set the label as the central widget
+    window.setCentralWidget(label);
+
+    // Set the window title
+    window.setWindowTitle("Qt Hello World");
+
+    // Set a reasonable default size
+    window.resize(400, 200);
+
+    // Show the window
     window.show();
 
+    // Run the application event loop
     return app.exec();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "SM83/sm83.hpp"
 #include "SM83/bus.hpp"
 #include "SM83/ram.hpp"
+#include "UI/debugger/debugger.hpp"
 
 int main() {
     std::cout << "Hello, Perkyello e Sal I'm alive!" << std::endl;
@@ -13,6 +14,8 @@ int main() {
     bus.attach_device(0x0000, 0xFFFF, &ram);
     sm83.run();
 
+
+    main_d(0, NULL);
 
     return 0;
 }
